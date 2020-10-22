f75b41b03087154b92cfebfb9c273f89245aff86
nuryildirim@gmail.com <43473199+nuryildirim@users.noreply.github.com>
2020-10-21 22:46:35 -0400

    Pydeck Map

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,9 @@ def get_coordinates(lat, lon):
     return coordinates_list
 
 
+
+
+
 @st.cache  # add caching so we load the data only once
 def load_data():  # Load the airbnb data acquired from InsideAirbnb.com
     # Server
@@ -267,3 +270,69 @@ if __name__ == "__main__":
         column1.write(view2)
         column2.write(view3)
         column3.write(view4)
+
+map_data = (
+    # "https://raw.githubusercontent.com/uber-common/"
+    # "deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv"
+    # "https://github.com/CMU-IDS-2020/a3-data-diggers/blob/master/data/2020/NYC/listings_09.csv"
+    # 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'
+    "https://raw.githubusercontent.com/CMU-IDS-2020/a3-data-diggers/master/data/2020/NYC/listings_09.csv"
+)
+
+
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=37.7749295,
+        longitude=-122.4,
+        bearing = 0,
+        zoom=11,
+        pitch=0,
+    ),
+    layers=[
+        pdk.Layer(
+            'ScreenGridLayer',
+            data=map_data,
+            pickable = False,
+            opacity = 0.8,
+            cell_size_pixels=50,
+            color_range=[
+                # [0, 25, 0, 25],
+                # [0, 85, 0, 85],
+                # [0, 127, 0, 127],
+                # [0, 170, 0, 170],
+                # [0, 190, 0, 190],
+                # [0, 255, 0, 255],
+                [252, 171, 143, 25],
+                [252, 138, 107, 85],
+                [249, 105, 76, 127],
+                [239, 70, 52, 170],
+                [217, 40, 36, 190],
+                [187, 22, 26, 255],
+            ],
+            get_position='[longitude, latitude]',
+            get_weight=2,
+            # get_position='[lon, lat]',
+            # radius=200,
+            # elevation_scale=4,
+            # elevation_range=[0, 1000],
+            # pickable=True,
+            # extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=map_data,
+            pickable = True,
+            opacity = 0.8,
+            filled=True,
+            radius_scale=0.8,
+            # radius_min_pixels=1,
+            # radius_max_pixels=100,
+            # get_position="coordinates",
+            get_position='[longitude, latitude]',
+            get_radius=20,
+            # get_fill_color=[200, 30, 0, 160],
+            get_fill_color=[32, 111, 178, 160],
+        ),
+    ],
+))
